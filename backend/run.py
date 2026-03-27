9539662bf943942162f15e1b295ef05fee026b09
KishorePrabakar <kishorefun21@gmail.com>
2026-03-27 18:05:42 +0530

    Backend hosting configs

--- backend/run.py
+++ backend/run.py
@@ -1,12 +1,14 @@
 # run.py
-import subprocess
-import sys
+import subprocess, sys, os
 
 if __name__ == "__main__":
-    subprocess.run([
+    is_dev = os.getenv("APP_ENV", "development") == "development"
+    cmd = [
         sys.executable, "-m", "uvicorn",
         "app.main:app",
         "--host", "0.0.0.0",
         "--port", "8000",
-        "--reload"
-    ])
+    ]
+    if is_dev:
+        cmd.append("--reload")
+    subprocess.run(cmd)
